b46325edb2d5040ec2c933b9c4bf85059224fe47
Romain <romain.trenet@gmail.com>
2023-10-12 17:55:33 +0200

    Update core and modules + add file system config (#22)

--- web/sites/dev/settings.php
+++ web/sites/dev/settings.php
@@ -522,7 +522,7 @@ $settings['update_free_access'] = FALSE;
  * See https://www.drupal.org/documentation/modules/file for more information
  * about securing private files.
  */
-# $settings['file_private_path'] = '';
+$settings['file_private_path'] = '../private';
 
 /**
  * Temporary file path:
@@ -535,7 +535,7 @@ $settings['update_free_access'] = FALSE;
  *
  * @see \Drupal\Component\FileSystem\FileSystem::getOsTemporaryDirectory()
  */
-# $settings['file_temp_path'] = '/tmp';
+$settings['file_temp_path'] = '../tmp';
 
 /**
  * Session write interval:
